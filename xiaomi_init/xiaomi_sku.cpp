27a0071605c6c107e3062da98e7de1307871a7f3
Ramii Ahmed <ramy@ahmedramy.com>
2023-08-31 08:53:38 +0000

    fixup

--- xiaomi_init/xiaomi_sku.cpp
+++ xiaomi_init/xiaomi_sku.cpp
@@ -1,10 +1,11 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * Copyright (C) 2023 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <android-base/logging.h>
+#define LOG_TAG "xiaomi_sku"
+
 #include <android-base/properties.h>
 
 #include <xiaomi_utils.h>
@@ -23,6 +24,7 @@ void search_variant(const std::vector<variant_info_t> variants) {
         if ((variant.hwc_value == "" || variant.hwc_value == hwc_value) &&
             (variant.sku_value == "" || variant.sku_value == sku_value)) {
             property_override(SKU_PROP, variant.sku);
+            LOG(INFO) << "Successfully set phone SKU ";
             break;
         }
     }
--- xiaomi_init/xiaomi_utils.cpp
+++ xiaomi_init/xiaomi_utils.cpp
@@ -1,8 +1,10 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * Copyright (C) 2023 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
+#define LOG_TAG "xiaomi_utils"
 
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
@@ -13,8 +15,18 @@
 void property_override(std::string prop, std::string value, bool add) {
     auto pi = (prop_info *) __system_property_find(prop.c_str());
     if (pi != nullptr) {
-        __system_property_update(pi, value.c_str(), value.length());
+       LOG(INFO) << "Setting property: " << prop.c_str() << " to " << value.c_str();
+
+        if (!__system_property_update(pi, value.c_str(), value.length())) {
+           LOG(ERROR) << "Unable to update property: " << prop.c_str() << " to " << value.c_str();
+           return -1;
+        }
     } else if (add) {
-        __system_property_add(prop.c_str(), prop.length(), value.c_str(), value.length());
+       LOG(INFO) << "Adding property: " << prop.c_str() << " with value " << value.c_str();
+
+        if (!__system_property_add(prop.c_str(), prop.length(), value.c_str(), value.length())) {
+           LOG(ERROR) << "Unable to add property: " << prop.c_str() << " with value " << value.c_str();
+           return -1;
+        }
     }
 }
